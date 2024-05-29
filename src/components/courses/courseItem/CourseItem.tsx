7b6faac461d5ac3185fae458b35b47f38e079a60
Eugene Klimenkov <eugeneklimenkov@Eugenes-MacBook-Pro.local>
2024-05-29 10:20:57 +0300

    add intersection-observer

--- src/components/courses/courseItem/CourseItem.tsx
+++ src/components/courses/courseItem/CourseItem.tsx
@@ -1,14 +1,18 @@
 import { FC, memo } from "react"
 import { IData } from "../../../types"
 import styles from "./courseItem.module.scss";
+import { useInView } from "react-intersection-observer";
 
 interface ICourseItemProps extends IData {}
 
 export const CourseItem: FC<ICourseItemProps> = memo(({name, bgColor, image}) => {
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
   return (
-    <div className={styles.container}>
+    <div ref={ref} className={styles.container}>
       <div style={{background: bgColor}} className={styles.backgroundWrapper}>
-        <img src={image} />
+        {inView && <img src={image} alt={name} />}
       </div>
       <div className={styles.text}>{name}</div>
     </div>
